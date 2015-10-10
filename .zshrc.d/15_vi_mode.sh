f109f8c51ff845935db56d1f997ca8f8cb7240dd
Brian Riehman <brian@riehman.com>
2015-10-10 08:49:59 -0500

    ZSH configuration updates

--- .zshrc.d/15_vi_mode.sh
+++ .zshrc.d/15_vi_mode.sh
@@ -4,13 +4,10 @@ bindkey -v
 # map jj as the esc key for vim mode
 bindkey "jj" vi-cmd-mode
 
-# map s the esc key for vim mode
-# bindkey "
-
 # Make Ctrl-P and Ctrl-N search the way I would expect in history
-autoload up-line-or-beginning-search
-autoload down-line-or-beginning-search
-zle -N up-line-or-beginning-search
-zle -N down-line-or-beginning-search
-bindkey '^P' up-line-or-beginning-search
-bindkey '^N' down-line-or-beginning-search
+bindkey '^P' history-incremental-search-backward
+bindkey '^N' history-incremental-search-forward
+
+# Let jj break from search mode
+bindkey -M isearch 'jj' accept-search
+bindkey -M isearch 'jj' accept-search
