905915cd839671dad535024e4b41d263b0f1e258
Brian Riehman <brian@riehman.com>
2015-10-11 19:14:48 -0500

    Add sudo helper function

---
+++ .zshrc.d/sudo.plugin.zsh
@@ -0,0 +1,27 @@
+# ------------------------------------------------------------------------------
+# Description
+# -----------
+#
+# sudo will be inserted before the command
+#
+# ------------------------------------------------------------------------------
+# Authors
+# -------
+#
+# * Dongweiming <ciici123@gmail.com>
+#
+# ------------------------------------------------------------------------------
+
+sudo-command-line() {
+    [[ -z $BUFFER ]] && zle up-history
+    if [[ $BUFFER == sudo\ * ]]; then
+        LBUFFER="${LBUFFER#sudo }"
+    else
+        LBUFFER="sudo $LBUFFER"
+    fi
+}
+
+zle -N sudo-command-line
+
+# Bind to ctrl-s
+bindkey '^s' sudo-command-line
