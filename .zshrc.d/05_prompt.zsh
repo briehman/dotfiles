4eeb341e1ad2cd4ddad85c2b1442e01ddc9e57b0
Brian Riehman <brian@riehman.com>
2015-10-16 19:10:29 -0500

    [zsh] Change prompt colors

--- .zshrc.d/05_prompt.zsh
+++ .zshrc.d/05_prompt.zsh
@@ -1,4 +1,4 @@
 autoload -Uz promptinit && promptinit
 autoload -Uz colors && colors
-export PROMPT=$'%{$fg[cyan]%}%n%{$reset_color%}@%{$fg[red]%}%m%{$reset_color%} %{$fg[green]%}%d%{$reset_color%}\n%(#.#.$) '
+export PROMPT=$'%{$fg[cyan]%}%n%{$reset_color%}@%{$fg[yellow]%}%m%{$reset_color%} %{$fg[green]%}%d%{$reset_color%}\n%(#.#.$) '
 
