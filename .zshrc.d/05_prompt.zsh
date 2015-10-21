d2626bc9a32d3661ba1e71a77699f7782d73e63c
Brian Riehman <brian@riehman.com>
2015-10-20 22:58:48 -0500

    [zsh] Color prompt

--- .zshrc.d/05_prompt.zsh
+++ .zshrc.d/05_prompt.zsh
@@ -1,4 +1,10 @@
 autoload -Uz promptinit && promptinit
 autoload -Uz colors && colors
-export PROMPT=$'%{$fg[cyan]%}%n%{$reset_color%}@%{$fg[yellow]%}%m%{$reset_color%} %{$fg[green]%}%d%{$reset_color%}\n%(#.#.$) '
+
+PROMPT_HOST='%{%K{black}%F{white}%} %n %{%F{black}%K{blue}%}⮀ %{%F{white}%}%m %{%F{blue}%K{yellow}%}'
+PROMPT_DIR='⮀%{%F{black}%} %1~ '
+PROMPT_SU='%(!.%{%k%F{blac}%K{yellow}%}⮀%{%F{black}%} ⚡ %{%k%F{black}%}.%{%k%F{yellow}%})⮀%{%f%k%b%}'
+
+PROMPT='%{%f%b%k%}
+$PROMPT_HOST$PROMPT_DIR$PROMPT_SU '
 
