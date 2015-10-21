8c00e741474cdb734ae7141ef72a0ad5b962b8a6
Brian Riehman <brian@riehman.com>
2015-10-21 07:42:10 -0500

    Add font documentation

--- .zshrc.d/05_prompt.zsh
+++ .zshrc.d/05_prompt.zsh
@@ -1,6 +1,8 @@
 autoload -Uz promptinit && promptinit
 autoload -Uz colors && colors
 
+# These special unicode characters require a system font that supports them:
+# https://powerline.readthedocs.org/en/latest/installation/linux.html#fonts-installation
 PROMPT_HOST='%{%K{black}%F{white}%} %n %{%F{black}%K{blue}%}⮀ %{%F{white}%}%m %{%F{blue}%K{yellow}%}'
 PROMPT_DIR='⮀%{%F{black}%} %1~ '
 PROMPT_SU='%(!.%{%k%F{blac}%K{yellow}%}⮀%{%F{black}%} ⚡ %{%k%F{black}%}.%{%k%F{yellow}%})⮀%{%f%k%b%}'
