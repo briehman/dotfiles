c08e5738ff7187caa54d02f55ad5750f3f3332c2
Brian Riehman <brian@riehman.com>
2016-09-23 19:25:23 -0500

    [zsh] Comment regarding variable expansion

--- .zshrc.d/random.zsh
+++ .zshrc.d/random.zsh
@@ -2,3 +2,8 @@
 
 # Press alt-q to push the current line on the buffer stack
 bindkey '\eq' push-line-or-edit
+
+# Probably do not want this on, but in case I think I do again, see below.
+# Split on words (spaces) for use in for loops. You can get around this with
+# ${=var}. See http://zsh.sourceforge.net/FAQ/zshfaq03.html for more detail.
+#setopt shwordsplit
