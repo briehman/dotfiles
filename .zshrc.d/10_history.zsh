c3dcc7c40e7c2007aa3aeceb52fda8d5cbc908b2
Brian Riehman <brian@riehman.com>
2017-07-15 19:22:26 -0500

    [Zsh] Increase zsh history

--- .zshrc.d/10_history.zsh
+++ .zshrc.d/10_history.zsh
@@ -1,7 +1,8 @@
 # Ignore duplicates and spaces, share history between sessions
 setopt inc_append_history hist_ignore_space histignorealldups sharehistory
 
-# Keep 10000 lines of history within the shell and save it to ~/.zsh_history:
-HISTSIZE=10000
-SAVEHIST=10000
+# Keep N lines of history within the shell and save it to ~/.zsh_history:
+HISTORY_IGNORE="(ls|cd|pwd|exit|cd ..)"
+HISTSIZE=100000
+SAVEHIST=100000
 HISTFILE=~/.zsh_history
