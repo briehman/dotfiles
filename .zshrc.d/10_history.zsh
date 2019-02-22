9076d8d0364535f0d793127f240c5ac75338db2a
Brian Riehman <brian@riehman.com>
2019-02-22 09:15:13 -0600

    [Zsh] Reduce zsh history size to speed up history searching

--- .zshrc.d/10_history.zsh
+++ .zshrc.d/10_history.zsh
@@ -3,6 +3,6 @@ setopt inc_append_history hist_ignore_space histignorealldups sharehistory
 
 # Keep N lines of history within the shell and save it to ~/.zsh_history:
 HISTORY_IGNORE="(ls|cd|pwd|exit|cd ..)"
-HISTSIZE=100000
-SAVEHIST=100000
+HISTSIZE=10000
+SAVEHIST=10000
 HISTFILE=~/.zsh_history
