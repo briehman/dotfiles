f7359c2bf32a5f40fcac6ae1b4553fc2ffbe4033
Brian Riehman <brian@riehman.com>
2019-10-01 09:01:00 -0500

    [Zsh] Display background job count in prompt

--- .zshrc.d/05_prompt.zsh
+++ .zshrc.d/05_prompt.zsh
@@ -4,8 +4,8 @@ autoload -Uz colors && colors
 # These special unicode characters require a system font that supports them:
 # https://powerline.readthedocs.org/en/latest/installation/linux.html#fonts-installation
 PROMPT_HOST='%{%K{black}%F{white}%} %n %{%F{black}%K{blue}%}⮀ %{%F{white}%}%m %{%F{blue}%K{yellow}%}'
-PROMPT_DIR='⮀%{%F{black}%} %1~ '
-PROMPT_SU='%(!.%{%k%F{blac}%K{yellow}%}⮀%{%F{black}%} ⚡ %{%k%F{black}%}.%{%k%F{yellow}%})⮀%{%f%k%b%}'
+PROMPT_DIR='⮀%{%F{red}%K{yellow}%} %1(j.%j .)%F{black}%} %1~ '
+PROMPT_SU='%(!.%{%k%F{black}%K{yellow}%}⮀%{%F{black}%} ⚡ %{%k%F{black}%}.%{%k%F{yellow}%})⮀%{%f%k%b%}'
 
 PROMPT='%{%f%b%k%}
 $PROMPT_HOST$PROMPT_DIR$PROMPT_SU '
